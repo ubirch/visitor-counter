bb0cb1358989e2405c75cc5229be920fbec6d854
derMicha <dermicha@ubirch.com>
2019-10-07 18:28:43 +0200

    added -f to rm

--- bin/scanWlan.sh
+++ bin/scanWlan.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 DATAFILEPREFIX=/tmp/crackdump*
-rm ${DATAFILEPREFIX}*
+
+rm -f ${DATAFILEPREFIX}*
 
 sudo airmon-ng start wlan1
 sudo airodump-ng --uptime --manufacturer --showack --beacons --berlin 20 --output-format csv --write /tmp/crackdump --write-interval 10 wlan1
