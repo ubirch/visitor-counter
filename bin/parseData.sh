606442db618742da3ef680fb12af15dddc65be30
derMicha <dermicha@ubirch.com>
2019-10-07 18:33:56 +0200

    fixed data scanner script

--- bin/parseData.sh
+++ bin/parseData.sh
@@ -5,6 +5,27 @@ function usage {
     exit 1
 }
 
+if [[ ! -z $1 ]]; then
+    ENV=$1
+    shift
+else
+    usage
+fi
+
+if [[ ! -z $1 ]]; then
+    CID=$1
+    shift
+else
+    usage
+fi
+
+if [[ ! -z $1 ]]; then
+    PWD=$1
+    shift
+else
+    usage
+fi
+
 DATAFILE=/tmp/crackdump-01.csv
 
-PARSER="tail -f python3 ./src/visitorCounter.py -env $ENV -cid $CID -pw $PWD"
+tail -f python3 ./src/visitorCounter.py -env $ENV -cid $CID -pw $PWD
