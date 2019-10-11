ba2d2dff071c325c394cd46c363ddd641e486804
derMicha <dermicha@ubirch.com>
2019-10-11 17:12:10 +0200

    added missing sudo

--- bin/scanWlan.sh
+++ bin/scanWlan.sh
@@ -2,7 +2,7 @@
 
 DATAFILEPREFIX=/tmp/crackdump*
 
-rm -f ${DATAFILEPREFIX}*
+sudo rm -f ${DATAFILEPREFIX}*
 
 sudo airmon-ng start wlan1
 sudo airodump-ng --band abg --ignore-negative-one --uptime --manufacturer --showack --beacons --berlin 20 --output-format csv --write /tmp/crackdump --write-interval 10 wlan1
